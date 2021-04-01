6870b45b0c73f39ca4ca3ffd495e45dcb65fb04b
David Dinhofer <daviddinhofer@Davids-MBP-2.hsd1.mi.comcast.net>
2021-04-01 16:22:34 -0400

    Second Commit

--- Project2.py
+++ Project2.py
@@ -99,7 +99,30 @@ def summarize_best_books(filepath):
     ("Fiction", "The Testaments (The Handmaid's Tale, #2)", "https://www.goodreads.com/choiceawards/best-fiction-books-2020") 
     to your list of tuples.
     """
-    
+    f = open(filepath)
+    html = f.read()
+    f.close()
+    categories = []
+    titles = []
+    urls = []
+    tups = []
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup.find_all('div', class_="category clearFix")
+    for tag in tags:
+        url_tag = tag.find_next('a')
+        url = url_tag.get('href')
+        cat = url_tag.find('h4').text.strip()
+        urls.append(url)
+        categories.append(cat)
+    title_tags = soup.find_all('img', class_="category__winnerImage")
+    for tag in title_tags:
+        title = tag.get('alt')
+        titles.append(title)
+    url_tags = soup.find_all('a', class_="category__copy")
+
+    for i in range(20):
+        tups.append((categories[i], titles[i], urls[i]))
+    return tups
 
 def write_csv(data, filename):
     """
@@ -121,7 +144,15 @@ def write_csv(data, filename):
 
     This function should not return anything.
     """
-    
+    out = open(filename, 'w')
+    out.write('Book title,Author Name')
+    out.write('\n')
+    for tup in data:
+        out.write(tup[0] + ', ' + tup[1])
+        out.write('\n')
+    out.close()
+    return None 
+
 
 def extra_credit(filepath):
     """
